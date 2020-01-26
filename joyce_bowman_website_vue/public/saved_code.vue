089a4f25247399ad161dd1dff3d9bc243f96ff3c
Cleveland Cooperative <cleveland.cooperative@gmail.com>
2020-01-25 19:03:26 -0500

    install template - initial work

---
+++ joyce_bowman_website_vue/public/saved_code.vue
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Joyce Bowman</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+  </head>
+  <body class="is-preload">
+    <div id="app"></div>
+    
+
+
+   
+
+
+
+      </div>
+
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/animate.min.css" rel="stylesheet">
+    <link href="/css/prettyPhoto.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/main.css" />
+
+
+    <!-- Scripts -->
+      <script src="/assets/js/jquery.min.js"></script>
+      <script src="/assets/js/jquery.poptrox.min.js"></script>
+      <script src="/assets/js/browser.min.js"></script>
+      <script src="/assets/js/breakpoints.min.js"></script>
+      <script src="/assets/js/util.js"></script>
+      <script src="/assets/js/main.js"></script>
+
+  </body>
+</html>
--- joyce_bowman_website_vue/src/App.vue
+++ joyce_bowman_website_vue/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+    <header id="header">
+    </header>
+
     <router-view/>
+
+        <!-- Footer -->
+          <section id="footer">
+            <section>
+              <ul class="icons">
+                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+                <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+              </ul>
+            </section>
+          </section>
+  
   </div>
 </template>
 
-<style>
-</style>
